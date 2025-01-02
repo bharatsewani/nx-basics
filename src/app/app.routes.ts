15f12fc032c27d657e865b30023b878ecbe4bd0a
techstuffwithbharat <bharatsewani.tech@gmail.com>
2025-01-02 13:17:38 +0530

    Initial commit of Nx Angular Code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Route } from '@angular/router';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+      path: '',
+      component: NxWelcomeComponent,
+      pathMatch: 'full',
+    },
+    {
+      path: 'products',
+      loadComponent: () =>
+        import('@nx-angular/products').then((m) => m.ProductsComponent),
+    },
+    {
+        path: 'orders',
+        loadComponent: () =>
+          import('@nx-angular/orders').then((m) => m.OrdersComponent),
+      },
+  ];
